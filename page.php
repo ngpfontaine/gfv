a779156683e7bec3b828f49fb60224ea10fbb61a
Nic Fontaine <ngpfontaine@gmail.com>
2016-04-05 11:57:55 -0400

    options for php, or wordpress page

--- page.php
+++ page.php
@@ -1,3 +1,9 @@
-<?php echo do_shortcode('[gravityform id=1'); ?>
+<!-- PAGE -->
+[gravityform id=1]
+<div id="form-button-cover">SUBMIT</div>
+<div id="form-confirmation"></div>
+
+<!-- PHP -->
+<?php echo do_shortcode('[gravityform id=1]'); ?>
 <div id="form-button-cover">SUBMIT</div>
 <div id="form-confirmation"></div>
